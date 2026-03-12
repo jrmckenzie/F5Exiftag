290efda2fffde9fac2cd5c42148b80f5de556935
jrm <j.r.mckenzie@gmail.com>
2026-03-12 17:44:56 +0000

    code cleanup

--- main.py
+++ main.py
@@ -39,11 +39,45 @@ sg.theme('SystemDefault')
 config = configparser.ConfigParser()
 script_path = Path(__file__).absolute().parent
 path_to_config = script_path / 'config.ini'
-config.read(path_to_config)
-# Read configuration and find location of Nikon Shooting Data folder, or ask user to set it
-if config.has_option('NikonSData', 'path'):
-    shooting_data_path = config.get('NikonSData', 'path')
-else:
+
+def licence_popup():
+    sg.popup("""This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.""",
+    title="Licence", line_width=80)
+    return True
+
+def about_popup():
+    sg.popup("""This is a tool for Nikon F5 camera users who connect their
+camera to a PC or Mac with a serial cable and use Nikon
+Photo Secretary for F5 to save the Shooting Data from a
+roll of film.
+
+You need to use the "Convert Data" function in Photo
+Secretary for F5 to save the shooting data in a text
+format readable by this software.
+
+You also need your film scans all saved as JPG and
+named according to a strict naming convention. e.g. if
+your Shooting Data is saved in a file named
+"2550103.txt" then the JPG files must be named
+"2550103-1.JPG", "2550103-2.JPG" etc.""",
+    "Version " + version_number + " / " + version_date + "\n" +
+    "Copyright © " + version_date[-4:] + " JR McKenzie (jrmknz@yahoo.co.uk)\n" +
+    "https://github.com/jrmckenzie/F5Exiftag\n",
+    title="About F5Exiftag")
+    return True
+
+def set_shooting_data_dir():
     loclayout = [[sg.T('')],
                  [sg.Text('Please locate your Nikon Shooting Data folder:'), sg.Input(key='-IN2-', change_submits=False,
                                                                                       readonly=True),
@@ -77,23 +111,114 @@ else:
             break
     locwindow.close()
 
-fd_layout = [[sg.Text('F5Exiftag - Tag Nikon F5 film scans with EXIF data', font=('Arial', 12, 'bold'))],
-             [sg.Text('Tag a batch of scanned files (in jpeg format) with lens data, using the Shooting Data exported' +
-                    '  from Nikon Photo Secretary AC-1WE for F5 updated by the user with lens info', size=(45, 5))],
-             [sg.Text('Please locate your Nikon Shooting Data file:')],
-             [sg.Input(key='-IN3-', change_submits=False, readonly=True),
-              sg.FileBrowse(key='FDloc', initial_folder=config.get('NikonSData', 'path'),
-                            file_types=(('Text documents', '*.txt'), ('All Files', '*.*')))],
-             [sg.Text('Please locate your Scanned Images folder:')],
-             [sg.Input(key='-IN4-', change_submits=False, readonly=True),
-            sg.FolderBrowse(key='SIloc', initial_folder=config.get('ScannedImagesPath', 'path')), ],
-             [sg.Button('Go!'), sg.Button('About'), sg.Button('Licence'), sg.Button('Exit')],
-             [sg.Text('v' + version_number + ' Copyright © ' + version_date[-4:] + ' JR McKenzie',
-                    font=('Arial', 8, 'normal'))],
-             ]
+
+def make_filmdata_window(program_title, program_desc, my_file_type):
+    fd_layout = [[sg.Text('F5Exiftag - Tag Nikon F5 film scans with EXIF data', font=('Arial', 12, 'bold'))],
+                 [sg.Text(program_desc, size=(45, 5))],
+                 [sg.Text('Please locate your Nikon Shooting Data file:')],
+                 [sg.Input(key='-IN3-', change_submits=False, readonly=True),
+                  sg.FileBrowse(key='FDloc', initial_folder=config.get('NikonSData', 'path'),
+                                file_types=((my_file_type), ('All Files', '*.*')))],
+                 [sg.Text('Please locate your Scanned Images folder:')],
+                 [sg.Input(key='-IN4-', change_submits=False, readonly=True),
+                  sg.FolderBrowse(key='SIloc', initial_folder=config.get('ScannedImagesPath', 'path')), ],
+                 [sg.Button('Go!'), sg.Button('About'), sg.Button('Licence'), sg.Button('Exit')],
+                 [sg.Text('v' + version_number + ' Copyright © ' + version_date[-4:] + ' JR McKenzie',
+                          font=('Arial', 8, 'normal'))],
+                 ]
+    return sg.Window(program_title, fd_layout)
+
+
+def save_tags_dict(sd_data_file):
+    sd_data_db = pd.read_csv(sd_data_file, header=1)
+    # Iterate through the frames on the film - pop up a progress bar window
+    progress_layout = [
+        [sg.Text('Processing scanned images')],
+        [sg.ProgressBar(1, orientation='h', key='progress', size=(25, 15))]
+    ]
+    progress_win = sg.Window('Processing...', progress_layout, disable_close=True).Finalize()
+    progress_win.bring_to_front()
+    progress_win.force_focus()
+    progress_bar = progress_win.find_element('progress')
+    for _, row in sd_data_db.iterrows():
+        progress_bar.UpdateBar(row['Frame Count'], len(sd_data_db))
+        ShutterSpeed = str(row['Shutter Speed'])
+        if ShutterSpeed[-1:] == "'":
+            ShutterSpeed = 60 * float(row['Shutter Speed'][:-1])
+        elif ShutterSpeed[-1:] != '"':
+            ShutterSpeed = "1/" + ShutterSpeed
+        else:
+            ShutterSpeed = row['Shutter Speed']
+        Focal_Length = str(row['Focal Length'])
+        tags_dict = {'Model': my_camera_model,
+                     'Lens': Focal_Length + 'mm f/' + str(row['Max. Aperture'])[1:],
+                     'ISO': ISO,
+                     'FocalLength': Focal_Length,
+                     'FocalLengthIn35mmFormat': Focal_Length,
+                     'FNumber': row['Aperture'][1:],
+                     'ExposureTime': ShutterSpeed,
+                     'SerialNumber': my_camera_serial_number
+                     }
+        if 'Metering System' in row:
+            if row['Metering System'][-6:] == 'Matrix':
+                MeteringMode = 'Multi-segment'
+            elif row['Metering System'] == 'Center-weighted':
+                MeteringMode = 'Center-weighted average'
+            else:
+                MeteringMode = 'Spot'
+            tags_dict.update({'MeteringMode': MeteringMode})
+        if 'Day' in row:
+            mydate = row['Day'].split('/')
+            DateTimeOriginal = mydate[2] + ':' + mydate[0].zfill(2) + ':' + mydate[1].zfill(2) + ' ' + row['Time']
+            tags_dict.update({'DateTimeOriginal': DateTimeOriginal,
+                              'CreateDate': DateTimeOriginal,
+                              'DateCreated': DateTimeOriginal})
+        if 'Exposure Mode' in row:
+            if row['Exposure Mode'] == 'Aperture-Priority Auto':
+                ExposureProgram = 'Aperture-priority AE'
+            elif row['Exposure Mode'] == 'Manual Exposure':
+                ExposureProgram = 'Manual'
+            elif row['Exposure Mode'] == 'Program Exposure':
+                ExposureProgram = 'Program AE'
+            else:
+                ExposureProgram = 'Shutter speed priority AE'
+            tags_dict.update({'ExposureProgram': ExposureProgram})
+        if 'Exposure Comp.' in row:
+            tags_dict.update({'ExposureCompensation': row['Exposure Comp.']})
+        ScanImageStem = str(sd_data_file.stem) + "-" + str(row['Frame Count'])
+        ScanImageRoot = Path(config.get('ScannedImagesPath', 'path'))
+        ScanImagePath = Path(ScanImageRoot / ScanImageStem).with_suffix('.JPG')
+        if Path(ScanImageRoot / ScanImageStem).with_suffix('.jpg').is_file():
+            ScanImagePath = Path(ScanImageRoot / ScanImageStem).with_suffix('.jpg')
+        if ScanImagePath.is_file():
+            with ExifToolHelper() as et:
+                et.set_tags(
+                    ScanImagePath,
+                    tags=tags_dict,
+                    params=["-P", "-overwrite_original"]
+                )
+        else:
+            sg.popup_error('Scan image could not be found in ' + str(ScanImagePath),
+                           'Are the scanned images saved in the right place and with the correct naming ' +
+                           'convention? This application will keep going and try the next one in the ' +
+                           'sequence until the end of the roll.', title='Error: scanned image not found.')
+    progress_win.close()
+    return True
+
+
+config.read(path_to_config)
+# Read configuration and find location of Nikon Shooting Data folder, or ask user to set it
+if config.has_option('NikonSData', 'path'):
+    shooting_data_path = config.get('NikonSData', 'path')
+else:
+    set_shooting_data_dir()
 
 if __name__ == "__main__":
-    filmdata_window = sg.Window('F5Exiftag', fd_layout)
+    my_file_type = 'Shooting data text files', '*.txt'
+    my_desc = ('Tag a batch of scanned files (in jpeg format) with the Shooting Data exported from Nikon' +
+                    ' Photo Secretary AC-1WE for F5')
+    my_title = 'F5Exiftag'
+    filmdata_window = make_filmdata_window(my_title, my_desc, my_file_type)
     while True:
         event, values = filmdata_window.read()
         if event == sg.WIN_CLOSED:
@@ -109,38 +234,11 @@ if __name__ == "__main__":
         elif event == 'Exit':
             sys.exit()
         elif event == 'About':
-            sg.popup("""This is a tool for Nikon F5 camera users who connect their
-camera to a PC or Mac with a serial cable and use Nikon
-Photo Secretary for F5 to save the Shooting Data from a
-roll of film.
-
-You need to use the "Convert Data" function in Photo
-Secretary for F5 to save the shooting data in a text
-format readable by this software.
-
-You also need your film scans all saved as JPG and
-named according to a strict naming convention. e.g. if
-your Shooting Data is saved in a file named
-"2550103.txt" then the JPG files must be named
-"2550103-1.JPG", "2550103-2.JPG" etc.""",
-                     "Version " + version_number + " / " + version_date + "\n" +
-                     "Copyright © " + version_date[-4:] + " JR McKenzie (jrmknz@yahoo.co.uk)\n" +
-                     "https://github.com/jrmckenzie/F5Exiftag\n",
-                     title="About F5Exiftag")
+            about_popup()
+            continue
         elif event == 'Licence':
-            sg.popup("""This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.""",
-                     title="Licence", line_width=80)
+            licence_popup()
+            continue
         elif event == 'Go!':
             if values['FDloc'] is None or len(values['FDloc']) <= 1:
                 sg.popup('Please browse for the path to your Nikon Shooting Data file and try again.')
@@ -155,7 +253,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.""",
             with open(path_to_config, 'w') as iconfigfile:
                 config.write(iconfigfile)
                 iconfigfile.close()
-            sd_data_file = config.get("NikonFData", "path")
+            sd_data_file = values['FDloc']
             try:
                 firstline = linecache.getline(sd_data_file, 1)
                 reader = csv.reader(StringIO(firstline))
@@ -165,79 +263,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.""",
                 sg.popup('Error: Shooting Data file not found.')
                 sys.exit('Error: Shooting Data file not found.')
             sd_data_file = Path(sd_data_file)
-            sd_data_db = pd.read_csv(sd_data_file, header=1)
-            # Iterate through the frames on the film - pop up a progress bar window
-            progress_layout = [
-                [sg.Text('Processing scanned images')],
-                [sg.ProgressBar(1, orientation='h', key='progress', size=(25, 15))]
-            ]
-            progress_win = sg.Window('Processing...', progress_layout, disable_close=True).Finalize()
-            progress_win.bring_to_front()
-            progress_win.force_focus()
-            progress_bar = progress_win.find_element('progress')
-            for _, row in sd_data_db.iterrows():
-                progress_bar.UpdateBar(row['Frame Count'], len(sd_data_db))
-                ShutterSpeed = str(row['Shutter Speed'])
-                if ShutterSpeed[-1:] == "'":
-                    ShutterSpeed = 60 * float(row['Shutter Speed'][:-1])
-                elif ShutterSpeed[-1:] != '"':
-                    ShutterSpeed = "1/" + ShutterSpeed
-                else:
-                    ShutterSpeed = row['Shutter Speed']
-                Focal_Length = str(row['Focal Length'])
-                tags_dict = {'Model': my_camera_model,
-                             'Lens': Focal_Length + 'mm f/' + str(row['Max. Aperture'])[1:],
-                             'ISO': ISO,
-                             'FocalLength': Focal_Length,
-                             'FocalLengthIn35mmFormat': Focal_Length,
-                             'FNumber': row['Aperture'][1:],
-                             'ExposureTime': ShutterSpeed,
-                             'SerialNumber': my_camera_serial_number
-                             }
-                if 'Metering System' in row:
-                    if row['Metering System'][-6:] == 'Matrix':
-                        MeteringMode = 'Multi-segment'
-                    elif row['Metering System'] == 'Center-weighted':
-                        MeteringMode = 'Center-weighted average'
-                    else:
-                        MeteringMode = 'Spot'
-                    tags_dict.update({'MeteringMode': MeteringMode})
-                if 'Day' in row:
-                    mydate = row['Day'].split('/')
-                    DateTimeOriginal = mydate[2] + ':' + mydate[0].zfill(2) + ':' + mydate[1].zfill(2) + ' ' + row['Time']
-                    tags_dict.update({'DateTimeOriginal': DateTimeOriginal,
-                                      'CreateDate': DateTimeOriginal,
-                                      'DateCreated': DateTimeOriginal})
-                if 'Exposure Mode' in row:
-                    if row['Exposure Mode'] == 'Aperture-Priority Auto':
-                        ExposureProgram = 'Aperture-priority AE'
-                    elif row['Exposure Mode'] == 'Manual Exposure':
-                        ExposureProgram = 'Manual'
-                    elif row['Exposure Mode'] == 'Program Exposure':
-                        ExposureProgram = 'Program AE'
-                    else:
-                        ExposureProgram = 'Shutter speed priority AE'
-                    tags_dict.update({'ExposureProgram': ExposureProgram})
-                if 'Exposure Comp.' in row:
-                    tags_dict.update({'ExposureCompensation': row['Exposure Comp.']})
-                ScanImageStem = str(sd_data_file.stem) + "-" + str(row['Frame Count'])
-                ScanImageRoot = Path(config.get('ScannedImagesPath', 'path'))
-                ScanImagePath = Path(ScanImageRoot / ScanImageStem).with_suffix('.JPG')
-                if Path(ScanImageRoot / ScanImageStem).with_suffix('.jpg').is_file():
-                    ScanImagePath = Path(ScanImageRoot / ScanImageStem).with_suffix('.jpg')
-                if ScanImagePath.is_file():
-                    with ExifToolHelper() as et:
-                        et.set_tags(
-                            ScanImagePath,
-                            tags=tags_dict,
-                            params=["-P", "-overwrite_original"]
-                        )
-                else:
-                    sg.popup_error('Scan image could not be found in ' + str(ScanImagePath),
-                                   'Are the scanned images saved in the right place and with the correct naming ' +
-                                   'convention? This application will keep going and try the next one in the ' +
-                                   'sequence until the end of the roll.', title='Error: scanned image not found.')
-            progress_win.close()
+            tags_dict = save_tags_dict(sd_data_file)
             sg.popup_ok('Process complete for Shooting Data ' +
                         Path(config.get('NikonFData', 'path')).stem +
                         '.\nThe application will now close.', title='Process complete')
